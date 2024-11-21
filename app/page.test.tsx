42fd521b3ed961db760e21e31258f3905b6ea7ec
SihyeonHong <hongsihyeon179@gmail.com>
2024-11-21 11:32:28 +0900

    fix: testing-library setup

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,10 +1,9 @@
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import Page from "./page";
 
 describe("Page", () => {
   it("renders the page", () => {
     render(<Page />);
-    expect(screen.getByText("Get started by editing")).toBeInTheDocument();
+    expect(screen.getByText(/Get started by editing/i)).toBeInTheDocument();
   });
 });
